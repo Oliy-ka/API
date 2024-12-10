6bc8df4687dd12f1d57b086adbd83587a6c8b10d
Olya <oborysov@koverly.com>
2024-12-10 19:38:01 +0200

    fixed one more CORS problem

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(express.json({
 
 app.use('*', (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
